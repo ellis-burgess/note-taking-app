476447caf5a1e15d1a74227d712f4107934c887d
Ellis Burgess <burgessem@cardiff.ac.uk>
2023-04-20 21:59:33 +0100

    Add note moved to own page

---
+++ note-taking-app/Pages/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace note_taking_app;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
---
+++ note-taking-app/Pages/NewNote.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace note_taking_app;
+
+public partial class NewNote : ContentPage
+{
+	public NewNote()
+	{
+		InitializeComponent();
+        SubmitNoteBtn.Clicked += SubmitNote;
+        NewNoteBtn.Clicked += AddNote;
+        EditNoteBtn.Clicked += EditNote;
+
+    }
+
+    private void SubmitNote(object sender, EventArgs e)
+    {
+        editor.IsEnabled = false;
+        SubmitNoteBtn.IsEnabled = false;
+        SubmitNoteBtn.Text = "Submitted!";
+        EditNoteBtn.IsEnabled = true;
+    }
+
+    private void AddNote(object sender, EventArgs e)
+    {
+        editor.Text = null;
+        SubmitNoteBtn.IsEnabled = true;
+        SubmitNoteBtn.Text = "Submit Note";
+        EditNoteBtn.IsEnabled = false;
+    }
+
+    private void EditNote(object sender, EventArgs e)
+    {
+        editor.IsEnabled = true;
+        SubmitNoteBtn.IsEnabled = true;
+        SubmitNoteBtn.Text = "Submit Note";
+    }
+
+}
